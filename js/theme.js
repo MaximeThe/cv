689386a2846c283e5c69ff06c1d8f5aca8a6eea7
MaximeThe <theneau.maxime@gmail.com>
2022-03-23 19:46:16 +0100

    Compétence - Slider continue

--- js/theme.js
+++ js/theme.js
@@ -2,13 +2,6 @@
 
 const buttonTheme = document.querySelector("#clickTheme");
 
-
-/* On attache un écouteur d'evenement (on embauche un espion pour surveiller le click sur le bouton) avec addEventListener() https://developer.mozilla.org/fr/docs/Web/API/EventTarget/addEventListener prend en paramètre :
-
-- 1 : le type d'évènement à écouter (liste des evenements ici : https://developer.mozilla.org/fr/docs/Web/Events#listing_des_%C3%A9v%C3%A9nements)
-
- 2 : le handler -> la fonction qui sera appelée par l'espion si l'evenement à lieu !Bonne pratique on donne à la fonction un nom qui commence par handle..Quelquechose on ne met pas les parenthèses sinon la fonction s'execute tout de suite et nous on veux l'executer que si l'evenement à lieu*/
-
 buttonTheme.addEventListener("click", handleClick);
 
 // on ecrit le code du handler
