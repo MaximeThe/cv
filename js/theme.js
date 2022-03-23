19726d5668146d3a96e7386b247ddee7af474a5e
MaximeThe <theneau.maxime@gmail.com>
2022-03-23 10:26:45 +0100

    Click affichage moon et sun

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,18 @@
 // On récupere l'element sur lequel on veut placer un espion
+
 const buttonTheme = document.querySelector("#clickTheme");
 
-// on attache un écouteur d'evenement (on embauche un espion pour surveiller le click sur le bouton) avec addEventListener()
-// https://developer.mozilla.org/fr/docs/Web/API/EventTarget/addEventListener
-// prend en paramètre :
-//  - 1 : le type d'évènement à écouter (liste des evenements ici : https://developer.mozilla.org/fr/docs/Web/Events#listing_des_%C3%A9v%C3%A9nements)
-//  - 2 : le handler -> la fonction qui sera appelée par l'espion si l'evenement à lieu !
-// bonne pratique on donne à l fonction une nom qui commence par handle..Quelquechose
-// on ne met pas les parenthèses sinon la fonction s'execute tout de suite et nous on veux l'executer que si l'evenement à lieu
+
+/* On attache un écouteur d'evenement (on embauche un espion pour surveiller le click sur le bouton) avec addEventListener() https://developer.mozilla.org/fr/docs/Web/API/EventTarget/addEventListener prend en paramètre :
+
+- 1 : le type d'évènement à écouter (liste des evenements ici : https://developer.mozilla.org/fr/docs/Web/Events#listing_des_%C3%A9v%C3%A9nements)
+
+ 2 : le handler -> la fonction qui sera appelée par l'espion si l'evenement à lieu !Bonne pratique on donne à la fonction un nom qui commence par handle..Quelquechose on ne met pas les parenthèses sinon la fonction s'execute tout de suite et nous on veux l'executer que si l'evenement à lieu*/
 
 buttonTheme.addEventListener("click", handleClick);
 
 // on ecrit le code du handler
+
 function handleClick(event) {
     event.preventDefault();
 
@@ -20,5 +21,16 @@ function handleClick(event) {
     //on rajoute la class 'dark__theme' à notre valeur 'body'
     newsletter.classList.toggle('theme--dark');
 
+    // On recupére l'intérieur de la class de l'icon moon
+    const sun = document.querySelector('.fa-sun');
+    //fa-sun est caché par défaut, on ajoute une class pour annuler le display: none.
+    sun.classList.toggle('fa-sun-on');
+
+    // On recupére l'intérieur de la class de l'icon moon
+    const moon = document.querySelector('.fa-moon');
+    //on rajoute une class avec display none 
+    moon.classList.toggle('fa-moon-off');
+    
+
+
 };
-//ssss
