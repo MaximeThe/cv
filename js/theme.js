8b1dbd3bbe1a3e253bc693142b4a01bf2c6a9974
MaximeThe <theneau.maxime@gmail.com>
2022-03-31 20:58:48 +0200

    Prob localstroage-Début Responsisve V2

--- js/theme.js
+++ js/theme.js
@@ -1,34 +1,44 @@
 const theme = {
   init: function(){
-    const buttonTheme = document.querySelector("#clickTheme");
+    const buttonTheme = document.querySelector("#iconTheme");
     buttonTheme.addEventListener("click", theme.handleTheme);
+    theme.applyLocalStorageTheme();
   },
   handleTheme: function(event) {
     event.preventDefault();
-
     const theme = document.querySelector("body");
     const icon = document.querySelector("#iconTheme");
-    
+    let darkTheme;
      if (theme.classList.contains("theme-dark")) { // Pas de thème         
       theme.classList.remove("theme-dark");
       icon.classList.remove("fa-sun");
       icon.classList.add("fa-moon");
+      darkTheme = false;
 
      } else { // si y'a pas 'theme-dark' 
          theme.classList.add("theme-dark");
          icon.classList.remove("fa-moon");
          icon.classList.add("fa-sun");
-
-     }
-
-    // On recupére l'intérieur de la class de l'icon moon
-    //
-    //fa-sun est caché par défaut, on ajoute une class pour annuler le display: none.
-    //
-
-      // On recupére l'intérieur de la class de l'icon moon
-    //
-      //on rajoute une class avec display none 
-    //
+         darkTheme = true;
+         
+     };
+     theme.setThemeToLocalStorage(darkTheme);
+     
   },
+  setThemeToLocalStorage: function (darkTheme) {//entrée dans local storage 
+    const stringifyDarkTheme = JSON.stringify(darkThemeA);
+    localStorage.setItem("dark_theme", stringifyDarkTheme);
+  },
+  applyLocalStorageTheme: function () {//récupère l'entrée 'dark_theme' dans le local storage 
+      const StringifyIsDark = localStorage.getItem("dark_theme");
+      const isDark = JSON.parse(StringifyIsDark); 
+      if (isDark) {
+          theme.changeTheme();
+      }
+      const colorTheme = localStorage.getItem("colorTheme");
+      if (colorTheme) {
+          theme.changeColorTheme(colorTheme);
+      }
+  },
+  
 }
