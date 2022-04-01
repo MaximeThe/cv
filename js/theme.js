5b42474690646e2865b772bd6d665b9127668742
MaximeThe <theneau.maxime@gmail.com>
2022-04-01 15:36:44 +0200

    ajout bouton thème

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,26 @@
 const theme = {
   init: function(){
+    //Theme-dark
     const buttonTheme = document.querySelector("#iconTheme");
     buttonTheme.addEventListener("click", theme.handleTheme);
     theme.applyLocalStorageTheme();
+    
+    //Theme.Btn
+    const colorBtnList = document.querySelectorAll(".theme-button");
+    for (let colorbtnElmt of colorBtnList) {
+      colorbtnElmt.addEventListener("click", theme.handleThemeColorClick);
+  }
   },
+handleThemeColorClick: function (event) {
+  const idColor = event.currentTarget.id;
+  theme.changeColorTheme(idColor);
+},
+changeColorTheme: function (colorName) {
+
+  document.body.classList.remove("theme-red", "theme-green", "theme-blue");
+  document.body.add(colorName);
+
+},
   handleTheme: function(event) {
     event.preventDefault();
     const theme = document.querySelector("body");
@@ -33,7 +50,7 @@ const theme = {
       const StringifyIsDark = localStorage.getItem("dark_theme");
       const isDark = JSON.parse(StringifyIsDark); 
       if (isDark) {
-          theme.changeTheme();
+          theme.handleTheme();
       }
       const colorTheme = localStorage.getItem("colorTheme");
       if (colorTheme) {
