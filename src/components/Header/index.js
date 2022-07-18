2d0f48c08c2ae89167791e7a4e7c13428502888f
MaximeThe <theneau.maxime@gmail.com>
2022-07-18 18:58:19 +0200

    background

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import Description from './Description';
 import './style.scss';
 import Title from './Title';
+import Icon from '../../assets/image/fondCv.svg';
 
 function Header() {
   return (
     <header className="header">
+      <img src={Icon} className="header-img" alt="background"/>
       <Title />
       <Description />
     </header>
