2c384636b84b13da43ff2368f4eca7c3006fac9b
Maxime The <theneau.maxime@gmail.com>
2022-08-30 20:31:55 +0200

    video background

--- src/components/Cv/index.js
+++ src/components/Cv/index.js
@@ -1,16 +1,19 @@
 // == Import
-import Footer from '../Footer';
-import Header from '../Header';
-import Main from '../Main';
+// import Video from "../../assets/video.mp4";
 import './styles.scss';
 
 // == Composant
 function cV() {
   return (
     <div className="app">
-      <Header />
-      <Main />
-      <Footer />
+      <video autoPlay muted loop id="myVideo">
+        <source src={require('../../assets/video.mp4')} type="video/mp4" />
+      </video>
+      <div className="content">
+        <h1>Theneau Maxime</h1>
+        <h2>Developpeur web Front-End à Marseille</h2>
+        <p>In Progress</p>
+      </div>
     </div>
   );
 }
